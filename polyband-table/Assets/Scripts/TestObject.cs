8b77f0a90f75433198b4d9b3ef856dcd5c58a3ae
Antoine Facq <antoine.facq@gmail.com>
2022-01-20 09:41:28 +0100

    Working connection between server and unity

---
+++ polyband-table/Assets/Scripts/TestObject.cs
@@ -0,0 +1,28 @@
+﻿// unity c# code
+using Socket.Quobject.SocketIoClientDotNet.Client;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    private QSocket socket;
+
+    void Start()
+    {
+        Debug.Log("start");
+        socket = IO.Socket("http://localhost:5000");
+
+        socket.On(QSocket.EVENT_CONNECT, () => {
+            Debug.Log("Connected");
+            socket.Emit("chat", "test");
+        });
+
+        socket.On("chat", data => {
+            Debug.Log("data : " + data);
+        });
+    }
+
+    private void OnDestroy()
+    {
+        socket.Disconnect();
+    }
+}
