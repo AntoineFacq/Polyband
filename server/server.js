8b06734b5fe068202234d5b75619e49cc546e864
Alexandre MAZURIER <a.mazurier83@gmail.com>
2022-01-21 17:16:26 +0100

    First implementation of tablet, with call help and volume

--- server/server.js
+++ server/server.js
@@ -4,15 +4,22 @@ let io = require("socket.io")(http, {cors: {origin: '*'}});
 
 io.on('connection', (socket) => {
   console.log('user connected');
-  
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  
+
   socket.on('add-message', (message) => {
     io.emit('message', {type:'new-message', text: message});
-    console.log(message);    
+    console.log(message);
   });
+
+  socket.on('911 called', (message) => {
+    console.log("911 !");
+    io.emit('message', {type:'911 Call', text: "A student called 911 !"});
+    console.log(message);
+  });
+
 });
 
 http.listen(5000, () => {
