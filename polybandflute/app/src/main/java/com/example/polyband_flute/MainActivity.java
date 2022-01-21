5c365229e0f913bc1a8ba1bd60cb0c49c6a69de3
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2022-01-22 00:57:53 +0100

    Creation of Android Studio project (+ try to connect it to socket.io)

---
+++ polybandflute/app/src/main/java/com/example/polyband_flute/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.polyband_flute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://localhost:5000");
+        } catch (URISyntaxException e) {
+            System.out.println("Error connecting to the socket.");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSocket.connect();
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button clicked.");
+                String message = "I need help !";
+                mSocket.emit("911 called", message);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+    }
+}
