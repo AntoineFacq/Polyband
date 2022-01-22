9526a76e5927d56e3ac874aa028fb8902a965af0
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2022-01-22 14:59:49 +0100

    Android Studio connected to the server !

--- polybandflute/app/src/main/java/com/example/polyband_flute/MainActivity.java
+++ polybandflute/app/src/main/java/com/example/polyband_flute/MainActivity.java
@@ -1,31 +1,39 @@
 package com.example.polyband_flute;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import okhttp3.WebSocket;
 
 public class MainActivity extends AppCompatActivity {
 
     private Socket mSocket;
     {
         try {
-            mSocket = IO.socket("http://localhost:5000");
+            mSocket = IO.socket("http://172.31.240.1:5000");
         } catch (URISyntaxException e) {
-            System.out.println("Error connecting to the socket.");
+            e.printStackTrace();
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mSocket.connect();
+
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.button_id);
