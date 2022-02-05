2ff8adb7ec7d447a7a2dfc457e2a3cc3b9f27e63
AntoineFACQ <antoine.facq@gmail.com>
2022-02-06 00:50:33 +0100

    Revert MainController.cs

--- polyband-table/Assets/Scripts/MainController.cs
+++ polyband-table/Assets/Scripts/MainController.cs
@@ -1,25 +1,29 @@
 ﻿// unity c# code
-using System.Collections;
-using System.Collections.Generic;
+// using Assets.Scripts;
 using Socket.Quobject.SocketIoClientDotNet.Client;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainController : MonoBehaviour
 {
     private QSocket socket;
 
     public float MasterVolume = 1F;
-
-    public AudioClip Music;
-
+    public float masterVolumeSave = 1F;
+    public int trackSelected = -1;
+    private int trackSelectedSave = -1;
+    public Button helpButton;
+    public AudioClip[] musics;
+    public AudioSource trackAudioSource;
 
     void Start()
     {
         var ip = "http://192.168.184.50:5000";
+        Button btn = helpButton.GetComponent<Button>();
+        btn.onClick.AddListener(helpButtonClicked);
 
         Debug.Log("Starting connection to "+ip+"...");
         socket = IO.Socket(ip);
-
 
         socket.On(QSocket.EVENT_CONNECT, () => {
             Debug.Log("Connected to server");
@@ -36,24 +40,44 @@ public class MainController : MonoBehaviour
         });
 
         socket.On("select-track", track => {
-            Debug.Log("Try start music");
-            playMusic();
-            Debug.Log("MUSIC STARTED");
+            switch(track)
+            {
+                case "track-01":
+                    this.trackSelected = 0;
+                    break;
+                case "track-02":
+                    this.trackSelected = 1;
+                    break;
+                default:
+                    this.trackSelected = 2;
+                    break;
+            }
+            Debug.Log("Switch track to :" + track);
         });
 
-        //socket.On("add-message", data => {
-        //    Debug.Log("data : " + data);
-        // });
-
-
-
-
+    }
+    void helpButtonClicked()
+    {
+        socket.Emit("911 called", "SOS");
     }
 
-
-    public void playMusic()
+    private void Update()
     {
-        AudioSource.PlayClipAtPoint(Music, Camera.main.transform.position, 100);
+        if (this.trackSelected != trackSelectedSave)
+        {
+            if(this.trackAudioSource.clip != null)
+            {
+                this.trackAudioSource.Stop();
+            }
+            this.trackAudioSource.clip = musics[this.trackSelected];
+            this.trackAudioSource.Play();
+            this.trackSelected = trackSelectedSave;
+        }
+        if (this.MasterVolume != masterVolumeSave)
+        {
+            AudioListener.volume = this.MasterVolume;
+            this.MasterVolume = masterVolumeSave;
+        }
     }
 
     private void OnDestroy()
